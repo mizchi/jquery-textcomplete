4015072876541413ca2b58b465efc324339b0278
Koutarou Chikuba <miz404@gmail.com>
2015-08-20 18:56:05 +0900

    Prefer global.jQuery

--- dist/jquery.textcomplete.js
+++ dist/jquery.textcomplete.js
@@ -3,8 +3,12 @@
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
   } else if (typeof module === "object" && module.exports) {
-    var $ = require('jquery');
-    module.exports = factory($);
+    if (window.jQuery) {
+      module.exports = factory(jQuery);
+    } else {
+      var $ = require('jquery');
+      module.exports = factory($);
+    }
   } else {
     // Browser globals
     factory(jQuery);
